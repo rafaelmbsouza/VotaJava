a5713df8ad18e1f0990b37386872c38b59ab15e5
Miriã Bernardes <miriabernardes@pop-os.localdomain>
2020-08-07 21:26:42 -0300

    Implementando sistema de votação

--- VotaJava.java
+++ VotaJava.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class VotaJava{
 
-	public static void main(){
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
 		System.out.println("Quantos anos voce tem?");
+		int idade = scanner.nextInt();
+		
+		if(idade >= 18){
+			System.out.println("Voto Obrigatório");
+		} else {
+			System.out.println("Voto não é obrigatório");
+		}
+		
+		
 	}
 
 }
